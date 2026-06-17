a4124dd4b639ade50f6c3321550a2d9776a41eab
Sascha Wagentrotz <103500489+SaschaWagentrotz@users.noreply.github.com>
2026-06-17 10:24:29 +0200

    Adjusted colors to look more like Unreal

--- Plugins/ImGuiToolkit/Source/ImGuiToolkitBackend/Private/ImGuiToolkitSettings.cpp
+++ Plugins/ImGuiToolkit/Source/ImGuiToolkitBackend/Private/ImGuiToolkitSettings.cpp
@@ -12,63 +12,63 @@ UImGuiToolkitSettings::UImGuiToolkitSettings()
 {
 	Text = FromSRGB(230, 230, 230);
 	TextDisabled = FromSRGB(132, 137, 145);
-	TextLink = FromSRGB(45, 120, 225);
-	TextSelectedBg = FromSRGB(60, 120, 200, 100);
+	TextLink = FromSRGB(40, 103, 203);
+	TextSelectedBg = FromSRGB(40, 103, 203);
 
-	WindowBg = FromSRGB(16, 17, 19, 240);
-	ChildBg = FromSRGB(20, 21, 24, 240);
-	PopupBg = FromSRGB(22, 23, 26, 240);
+	WindowBg = FromSRGB(26, 26, 25);
+	ChildBg = FromSRGB(26, 26, 26);
+	PopupBg = FromSRGB(56, 55, 56);
 	TitleBg = FromSRGB(26, 27, 31);
-	TitleBgActive = FromSRGB(34, 36, 41);
+	TitleBgActive = FromSRGB(21, 21, 21);
 	TitleBgCollapsed = FromSRGB(0, 0, 0, 130);
-	MenuBarBg = FromSRGB(24, 26, 29);
+	MenuBarBg = FromSRGB(36, 35, 35);
 
-	Border = FromSRGB(56, 60, 66);
+	Border = FromSRGB(56, 60, 66, 0);
 	BorderShadow = FromSRGB(0, 0, 0, 0);
 
-	FrameBg = FromSRGB(34, 37, 42, 220);
+	FrameBg = FromSRGB(36, 36, 36);
 	FrameBgHovered = FromSRGB(42, 46, 52, 230);
 	FrameBgActive = FromSRGB(48, 53, 60);
 
-	Button = FromSRGB(52, 54, 58, 220);
-	ButtonHovered = FromSRGB(70, 73, 78);
-	ButtonActive = FromSRGB(35, 100, 210);
+	Button = FromSRGB(56, 55, 56);
+	ButtonHovered = FromSRGB(82, 82, 82);
+	ButtonActive = FromSRGB(57, 101, 196);
 
-	CheckMark = FromSRGB(45, 120, 225);
+	CheckMark = FromSRGB(40, 103, 203);
 	SliderGrab = FromSRGB(70, 145, 245);
-	SliderGrabActive = FromSRGB(35, 100, 210);
+	SliderGrabActive = FromSRGB(57, 101, 196);
 
-	Header = FromSRGB(45, 49, 56, 200);
-	HeaderHovered = FromSRGB(55, 60, 68, 230);
+	Header = FromSRGB(40, 103, 203);
+	HeaderHovered = FromSRGB(82, 82, 82);
 	HeaderActive = FromSRGB(60, 66, 75);
 
 	Separator = FromSRGB(56, 60, 66);
-	SeparatorHovered = FromSRGB(70, 145, 245);
-	SeparatorActive = FromSRGB(35, 100, 210);
+	SeparatorHovered = FromSRGB(40, 103, 203);
+	SeparatorActive = FromSRGB(57, 101, 196);
 
 	ResizeGrip = FromSRGB(45, 120, 225, 56);
-	ResizeGripHovered = FromSRGB(45, 120, 225, 184);
-	ResizeGripActive = FromSRGB(45, 120, 225, 242);
+	ResizeGripHovered = FromSRGB(40, 103, 203, 184);
+	ResizeGripActive = FromSRGB(40, 103, 203, 242);
 
-	Tab = FromSRGB(40, 44, 50);
-	TabHovered = FromSRGB(55, 60, 68);
-	TabSelected = FromSRGB(45, 120, 225);
-	TabSelectedOverline = FromSRGB(45, 120, 225);
+	Tab = FromSRGB(21, 21, 20);
+	TabHovered = FromSRGB(33, 33, 33);
+	TabSelected = FromSRGB(36, 35, 35);
+	TabSelectedOverline = FromSRGB(36, 35, 35);
 	TabDimmed = FromSRGB(38, 40, 44);
 	TabDimmedSelected = FromSRGB(45, 48, 53);
 	TabDimmedSelectedOverline = FromSRGB(0, 0, 0, 0);
 
-	DockingPreview = FromSRGB(45, 120, 225, 179);
+	DockingPreview = FromSRGB(40, 103, 203, 179);
 	DockingEmptyBg = FromSRGB(34, 36, 41);
 
-	ScrollbarBg = FromSRGB(16, 17, 19, 135);
-	ScrollbarGrab = FromSRGB(67, 72, 79);
-	ScrollbarGrabHovered = FromSRGB(86, 92, 100);
-	ScrollbarGrabActive = FromSRGB(104, 112, 122);
+	ScrollbarBg = FromSRGB(26, 26, 25, 135);
+	ScrollbarGrab = FromSRGB(56, 55, 56);
+	ScrollbarGrabHovered = FromSRGB(82, 82, 82);
+	ScrollbarGrabActive = FromSRGB(104, 104, 104);
 
-	TableHeaderBg = FromSRGB(40, 43, 49);
-	TableBorderStrong = FromSRGB(70, 74, 82);
-	TableBorderLight = FromSRGB(52, 56, 63);
+	TableHeaderBg = FromSRGB(36, 35, 35);
+	TableBorderStrong = FromSRGB(82, 82, 82);
+	TableBorderLight = FromSRGB(56, 55, 56);
 	TableRowBg = FromSRGB(0, 0, 0, 0);
 	TableRowBgAlt = FromSRGB(255, 255, 255, 16);
 
@@ -78,7 +78,7 @@ UImGuiToolkitSettings::UImGuiToolkitSettings()
 	PlotHistogramHovered = FromSRGB(255, 171, 51);
 
 	DragDropTarget = FromSRGB(255, 220, 0, 230);
-	NavCursor = FromSRGB(45, 120, 225);
+	NavCursor = FromSRGB(40, 103, 203);
 	NavWindowingHighlight = FromSRGB(255, 255, 255, 180);
 	NavWindowingDimBg = FromSRGB(0, 0, 0, 120);
 	ModalWindowDimBg = FromSRGB(0, 0, 0, 150);
--- Plugins/ImGuiToolkit/Source/ImGuiToolkitBackend/Public/ImGuiToolkitSettings.h
+++ Plugins/ImGuiToolkit/Source/ImGuiToolkitBackend/Public/ImGuiToolkitSettings.h
@@ -16,178 +16,178 @@ public:
 	float Scale = 2.0f;
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Text")
-	FLinearColor Text = FLinearColor(230.0f / 255.0f, 230.0f / 255.0f, 230.0f / 255.0f, 1.0f);
+	FLinearColor Text = FLinearColor::FromSRGBColor(FColor(230, 230, 230));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Text")
-	FLinearColor TextDisabled = FLinearColor(132.0f / 255.0f, 137.0f / 255.0f, 145.0f / 255.0f, 1.0f);
+	FLinearColor TextDisabled = FLinearColor::FromSRGBColor(FColor(132, 137, 145));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Text")
-	FLinearColor TextLink = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 1.0f);
+	FLinearColor TextLink = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Text")
-	FLinearColor TextSelectedBg = FLinearColor(60.0f / 255.0f, 120.0f / 255.0f, 200.0f / 255.0f, 100.0f / 255.0f);
+	FLinearColor TextSelectedBg = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor WindowBg = FLinearColor(16.0f / 255.0f, 17.0f / 255.0f, 19.0f / 255.0f, 240.0f / 255.0f);
+	FLinearColor WindowBg = FLinearColor::FromSRGBColor(FColor(26, 26, 25));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor ChildBg = FLinearColor(20.0f / 255.0f, 21.0f / 255.0f, 24.0f / 255.0f, 240.0f / 255.0f);
+	FLinearColor ChildBg = FLinearColor::FromSRGBColor(FColor(26, 26, 26));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor PopupBg = FLinearColor(22.0f / 255.0f, 23.0f / 255.0f, 26.0f / 255.0f, 240.0f / 255.0f);
+	FLinearColor PopupBg = FLinearColor::FromSRGBColor(FColor(56, 55, 56));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor TitleBg = FLinearColor(26.0f / 255.0f, 27.0f / 255.0f, 31.0f / 255.0f, 1.0f);
+	FLinearColor TitleBg = FLinearColor::FromSRGBColor(FColor(26, 27, 31));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor TitleBgActive = FLinearColor(34.0f / 255.0f, 36.0f / 255.0f, 41.0f / 255.0f, 1.0f);
+	FLinearColor TitleBgActive = FLinearColor::FromSRGBColor(FColor(21, 21, 21));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Windows")
-	FLinearColor TitleBgCollapsed = FLinearColor(0.0f, 0.0f, 0.0f, 130.0f / 255.0f);
+	FLinearColor TitleBgCollapsed = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 130));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Borders")
-	FLinearColor Border = FLinearColor(56.0f / 255.0f, 60.0f / 255.0f, 66.0f / 255.0f, 1.0f);
+	FLinearColor Border = FLinearColor::FromSRGBColor(FColor(56, 60, 66, 0));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Borders")
-	FLinearColor BorderShadow = FLinearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	FLinearColor BorderShadow = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 0));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Frames")
-	FLinearColor FrameBg = FLinearColor(34.0f / 255.0f, 37.0f / 255.0f, 42.0f / 255.0f, 220.0f / 255.0f);
+	FLinearColor FrameBg = FLinearColor::FromSRGBColor(FColor(36, 36, 36));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Frames")
-	FLinearColor FrameBgHovered = FLinearColor(42.0f / 255.0f, 46.0f / 255.0f, 52.0f / 255.0f, 230.0f / 255.0f);
+	FLinearColor FrameBgHovered = FLinearColor::FromSRGBColor(FColor(42, 46, 52, 230));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Frames")
-	FLinearColor FrameBgActive = FLinearColor(48.0f / 255.0f, 53.0f / 255.0f, 60.0f / 255.0f, 1.0f);
+	FLinearColor FrameBgActive = FLinearColor::FromSRGBColor(FColor(48, 53, 60));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Buttons")
-	FLinearColor Button = FLinearColor(52.0f / 255.0f, 54.0f / 255.0f, 58.0f / 255.0f, 220.0f / 255.0f);
+	FLinearColor Button = FLinearColor::FromSRGBColor(FColor(56, 55, 56));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Buttons")
-	FLinearColor ButtonHovered = FLinearColor(70.0f / 255.0f, 73.0f / 255.0f, 78.0f / 255.0f, 1.0f);
+	FLinearColor ButtonHovered = FLinearColor::FromSRGBColor(FColor(82, 82, 82));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Buttons")
-	FLinearColor ButtonActive = FLinearColor(35.0f / 255.0f, 100.0f / 255.0f, 210.0f / 255.0f, 1.0f);
+	FLinearColor ButtonActive = FLinearColor::FromSRGBColor(FColor(57, 101, 196));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Accent")
-	FLinearColor CheckMark = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 1.0f);
+	FLinearColor CheckMark = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Accent")
-	FLinearColor SliderGrab = FLinearColor(70.0f / 255.0f, 145.0f / 255.0f, 245.0f / 255.0f, 1.0f);
+	FLinearColor SliderGrab = FLinearColor::FromSRGBColor(FColor(70, 145, 245));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Accent")
-	FLinearColor SliderGrabActive = FLinearColor(35.0f / 255.0f, 100.0f / 255.0f, 210.0f / 255.0f, 1.0f);
+	FLinearColor SliderGrabActive = FLinearColor::FromSRGBColor(FColor(57, 101, 196));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Headers")
-	FLinearColor Header = FLinearColor(45.0f / 255.0f, 49.0f / 255.0f, 56.0f / 255.0f, 200.0f / 255.0f);
+	FLinearColor Header = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Headers")
-	FLinearColor HeaderHovered = FLinearColor(55.0f / 255.0f, 60.0f / 255.0f, 68.0f / 255.0f, 230.0f / 255.0f);
+	FLinearColor HeaderHovered = FLinearColor::FromSRGBColor(FColor(82, 82, 82));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Headers")
-	FLinearColor HeaderActive = FLinearColor(60.0f / 255.0f, 66.0f / 255.0f, 75.0f / 255.0f, 1.0f);
+	FLinearColor HeaderActive = FLinearColor::FromSRGBColor(FColor(60, 66, 75));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Separators")
-	FLinearColor Separator = FLinearColor(56.0f / 255.0f, 60.0f / 255.0f, 66.0f / 255.0f, 1.0f);
+	FLinearColor Separator = FLinearColor::FromSRGBColor(FColor(56, 60, 66));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Separators")
-	FLinearColor SeparatorHovered = FLinearColor(70.0f / 255.0f, 145.0f / 255.0f, 245.0f / 255.0f, 1.0f);
+	FLinearColor SeparatorHovered = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Separators")
-	FLinearColor SeparatorActive = FLinearColor(35.0f / 255.0f, 100.0f / 255.0f, 210.0f / 255.0f, 1.0f);
+	FLinearColor SeparatorActive = FLinearColor::FromSRGBColor(FColor(57, 101, 196));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Resize Grip")
-	FLinearColor ResizeGrip = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 0.22f);
+	FLinearColor ResizeGrip = FLinearColor::FromSRGBColor(FColor(45, 120, 225, 56));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Resize Grip")
-	FLinearColor ResizeGripHovered = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 0.72f);
+	FLinearColor ResizeGripHovered = FLinearColor::FromSRGBColor(FColor(40, 103, 203, 184));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Resize Grip")
-	FLinearColor ResizeGripActive = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 0.95f);
+	FLinearColor ResizeGripActive = FLinearColor::FromSRGBColor(FColor(40, 103, 203, 242));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor Tab = FLinearColor(40.0f / 255.0f, 44.0f / 255.0f, 50.0f / 255.0f, 1.0f);
+	FLinearColor Tab = FLinearColor::FromSRGBColor(FColor(21, 21, 20));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabHovered = FLinearColor(55.0f / 255.0f, 60.0f / 255.0f, 68.0f / 255.0f, 1.0f);
+	FLinearColor TabHovered = FLinearColor::FromSRGBColor(FColor(33, 33, 33));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabSelected = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 1.0f);
+	FLinearColor TabSelected = FLinearColor::FromSRGBColor(FColor(36, 35, 35));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabSelectedOverline = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 1.0f);
+	FLinearColor TabSelectedOverline = FLinearColor::FromSRGBColor(FColor(36, 35, 35));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabDimmed = FLinearColor(38.0f / 255.0f, 40.0f / 255.0f, 44.0f / 255.0f, 1.0f);
+	FLinearColor TabDimmed = FLinearColor::FromSRGBColor(FColor(38, 40, 44));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabDimmedSelected = FLinearColor(45.0f / 255.0f, 48.0f / 255.0f, 53.0f / 255.0f, 1.0f);
+	FLinearColor TabDimmedSelected = FLinearColor::FromSRGBColor(FColor(45, 48, 53));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tabs")
-	FLinearColor TabDimmedSelectedOverline = FLinearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	FLinearColor TabDimmedSelectedOverline = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 0));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Docking")
-	FLinearColor DockingPreview = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 0.70f);
+	FLinearColor DockingPreview = FLinearColor::FromSRGBColor(FColor(40, 103, 203, 179));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Docking")
-	FLinearColor DockingEmptyBg = FLinearColor(34.0f / 255.0f, 36.0f / 255.0f, 41.0f / 255.0f, 1.0f);
+	FLinearColor DockingEmptyBg = FLinearColor::FromSRGBColor(FColor(34, 36, 41));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Scrollbars")
-	FLinearColor ScrollbarBg = FLinearColor(16.0f / 255.0f, 17.0f / 255.0f, 19.0f / 255.0f, 135.0f / 255.0f);
+	FLinearColor ScrollbarBg = FLinearColor::FromSRGBColor(FColor(26, 26, 25, 135));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Scrollbars")
-	FLinearColor ScrollbarGrab = FLinearColor(67.0f / 255.0f, 72.0f / 255.0f, 79.0f / 255.0f, 1.0f);
+	FLinearColor ScrollbarGrab = FLinearColor::FromSRGBColor(FColor(56, 55, 56));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Scrollbars")
-	FLinearColor ScrollbarGrabHovered = FLinearColor(86.0f / 255.0f, 92.0f / 255.0f, 100.0f / 255.0f, 1.0f);
+	FLinearColor ScrollbarGrabHovered = FLinearColor::FromSRGBColor(FColor(82, 82, 82));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Scrollbars")
-	FLinearColor ScrollbarGrabActive = FLinearColor(104.0f / 255.0f, 112.0f / 255.0f, 122.0f / 255.0f, 1.0f);
+	FLinearColor ScrollbarGrabActive = FLinearColor::FromSRGBColor(FColor(104, 104, 104));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tables")
-	FLinearColor TableHeaderBg = FLinearColor(40.0f / 255.0f, 43.0f / 255.0f, 49.0f / 255.0f, 1.0f);
+	FLinearColor TableHeaderBg = FLinearColor::FromSRGBColor(FColor(36, 35, 35));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tables")
-	FLinearColor TableBorderStrong = FLinearColor(70.0f / 255.0f, 74.0f / 255.0f, 82.0f / 255.0f, 1.0f);
+	FLinearColor TableBorderStrong = FLinearColor::FromSRGBColor(FColor(82, 82, 82));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tables")
-	FLinearColor TableBorderLight = FLinearColor(52.0f / 255.0f, 56.0f / 255.0f, 63.0f / 255.0f, 1.0f);
+	FLinearColor TableBorderLight = FLinearColor::FromSRGBColor(FColor(56, 55, 56));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tables")
-	FLinearColor TableRowBg = FLinearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	FLinearColor TableRowBg = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 0));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Tables")
-	FLinearColor TableRowBgAlt = FLinearColor(1.0f, 1.0f, 1.0f, 16.0f / 255.0f);
+	FLinearColor TableRowBgAlt = FLinearColor::FromSRGBColor(FColor(255, 255, 255, 16));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Plots")
-	FLinearColor PlotLines = FLinearColor(156.0f / 255.0f, 156.0f / 255.0f, 156.0f / 255.0f, 1.0f);
+	FLinearColor PlotLines = FLinearColor::FromSRGBColor(FColor(156, 156, 156));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Plots")
-	FLinearColor PlotLinesHovered = FLinearColor(1.0f, 110.0f / 255.0f, 96.0f / 255.0f, 1.0f);
+	FLinearColor PlotLinesHovered = FLinearColor::FromSRGBColor(FColor(255, 110, 96));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Plots")
-	FLinearColor PlotHistogram = FLinearColor(229.0f / 255.0f, 178.0f / 255.0f, 59.0f / 255.0f, 1.0f);
+	FLinearColor PlotHistogram = FLinearColor::FromSRGBColor(FColor(229, 178, 59));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Plots")
-	FLinearColor PlotHistogramHovered = FLinearColor(1.0f, 171.0f / 255.0f, 51.0f / 255.0f, 1.0f);
+	FLinearColor PlotHistogramHovered = FLinearColor::FromSRGBColor(FColor(255, 171, 51));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Feedback")
-	FLinearColor DragDropTarget = FLinearColor(1.0f, 220.0f / 255.0f, 0.0f, 230.0f / 255.0f);
+	FLinearColor DragDropTarget = FLinearColor::FromSRGBColor(FColor(255, 220, 0, 230));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Navigation")
-	FLinearColor NavCursor = FLinearColor(45.0f / 255.0f, 120.0f / 255.0f, 225.0f / 255.0f, 1.0f);
+	FLinearColor NavCursor = FLinearColor::FromSRGBColor(FColor(40, 103, 203));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Navigation")
-	FLinearColor NavWindowingHighlight = FLinearColor(1.0f, 1.0f, 1.0f, 180.0f / 255.0f);
+	FLinearColor NavWindowingHighlight = FLinearColor::FromSRGBColor(FColor(255, 255, 255, 180));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Navigation")
-	FLinearColor NavWindowingDimBg = FLinearColor(0.0f, 0.0f, 0.0f, 120.0f / 255.0f);
+	FLinearColor NavWindowingDimBg = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 120));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Modals")
-	FLinearColor ModalWindowDimBg = FLinearColor(0.0f, 0.0f, 0.0f, 150.0f / 255.0f);
+	FLinearColor ModalWindowDimBg = FLinearColor::FromSRGBColor(FColor(0, 0, 0, 150));
 
 	UPROPERTY(Config, EditAnywhere, Category = "Colors|Menus")
-	FLinearColor MenuBarBg = FLinearColor(24.0f / 255.0f, 26.0f / 255.0f, 29.0f / 255.0f, 1.0f);
+	FLinearColor MenuBarBg = FLinearColor::FromSRGBColor(FColor(36, 35, 35));
 
 	virtual FName GetCategoryName() const override;
 	virtual FName GetSectionName() const override;
